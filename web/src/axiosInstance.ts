eaa80b0c376e7aa066fa97e7d9d0d465f4dbdd1f
Fernando Bonfim <devnoob@vivaldi.net>
2023-02-07 06:06:50 -0300

    Console.log process.env.API_BASE_URL

--- web/src/axiosInstance.ts
+++ web/src/axiosInstance.ts
@@ -1,5 +1,7 @@
 import axios, { AxiosInstance } from 'axios';
 
+console.log(process.env.API_BASE_URL);
+
 const api: AxiosInstance = axios.create({
   baseURL: process.env.API_BASE_URL || "http://localhost:3333",
 });
