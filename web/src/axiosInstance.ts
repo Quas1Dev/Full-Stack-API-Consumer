0bf7683ae7c689ea56ef0729def928493033d8e9
Fernando Bonfim <devnoob@vivaldi.net>
2023-02-07 07:13:43 -0300

    Logging import.meta.env.VITE_API_BASE_URL

--- web/src/axiosInstance.ts
+++ web/src/axiosInstance.ts
@@ -1,6 +1,6 @@
 import axios, { AxiosInstance } from 'axios';
 
-console.log("Server: ", process.env.API_BASE_URL);
+console.log("Server: ", import.meta.env.VITE_API_BASE_URL);
 
 const api: AxiosInstance = axios.create({
   baseURL: import.meta.env.VITE_API_BASE_URL || "http://localhost:3333",
