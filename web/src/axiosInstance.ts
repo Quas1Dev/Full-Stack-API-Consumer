fd463940c435dbbb676ec6035e9eacdd768dc210
Fernando Bonfim <devnoob@vivaldi.net>
2023-02-07 06:09:06 -0300

    Added text to mark server url

--- web/src/axiosInstance.ts
+++ web/src/axiosInstance.ts
@@ -1,6 +1,6 @@
 import axios, { AxiosInstance } from 'axios';
 
-console.log(process.env.API_BASE_URL);
+console.log("Server: ", process.env.API_BASE_URL);
 
 const api: AxiosInstance = axios.create({
   baseURL: process.env.API_BASE_URL || "http://localhost:3333",
