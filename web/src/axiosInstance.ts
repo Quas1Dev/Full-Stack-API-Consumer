eb3a3721bc6ab3e1bf37a4cb6ab458e03febb2ab
Fernando Bonfim <devnoob@vivaldi.net>
2023-02-07 05:37:13 -0300

    Fixed typo on env var API_BASE_UR

--- web/src/axiosInstance.ts
+++ web/src/axiosInstance.ts
@@ -1,7 +1,7 @@
 import axios, { AxiosInstance } from 'axios';
 
 const api: AxiosInstance = axios.create({
-  baseURL: process.env.API_BASE_UR || "http://localhost:3333",
+  baseURL: process.env.API_BASE_URL || "http://localhost:3333",
 });
 
 export default api;
