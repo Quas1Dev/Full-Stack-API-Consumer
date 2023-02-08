3c4a45da11ae230667dea8dd1e1ff483d3fbe432
Fernando Bonfim <devnoob@vivaldi.net>
2023-02-08 09:16:14 -0300

    Requiring password on env var

--- server/src/config/dbConfig.ts
+++ server/src/config/dbConfig.ts
@@ -1,7 +1,9 @@
 import mongoose from 'mongoose';
 mongoose.set('strictQuery', false);
 
-const uriToConnect = "mongodb+srv://usuario:usuario@cluster0.tiaomwt.mongodb.net/desafio?retryWrites=true&w=majority";
+const password = process.env.PASSWORD;
+
+const uriToConnect = `mongodb+srv://usuario:${password}@cluster0.tiaomwt.mongodb.net/desafio?retryWrites=true&w=majority`;
 
 const connection = mongoose.connect(uriToConnect);
 
