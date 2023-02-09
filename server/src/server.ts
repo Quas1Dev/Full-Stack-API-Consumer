f5721457bd793f83da2618c6be50e4399fecd101
Fernando Bonfim <devnoob@vivaldi.net>
2023-02-09 12:17:08 -0300

    Routes

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,11 +1,13 @@
 import express from 'express';
 import cors from 'cors';
 import './config/dbConfig'; // Config is executed
-import routes from './routes/routes';
+import clientsRoutes from './routes/clientsRoutes';
+import usersRoutes from './routes/userRoutes';
 
 const app = express();
 app.use(cors());
 app.use(express.json());
-app.use(routes);
+app.use(clientsRoutes);
+app.use(usersRoutes);
 
 app.listen(process.env.PORT || 3333);
