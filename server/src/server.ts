d31a3e2b08a7015aae6b9728c742ddc9ecacc40a
Fernando Bonfim <devnoob@vivaldi.net>
2023-02-09 13:54:56 -0300

    Added morgan logger

--- server/src/server.ts
+++ server/src/server.ts
@@ -3,8 +3,11 @@ import cors from 'cors';
 import './config/dbConfig'; // Config is executed
 import clientsRoutes from './routes/clientsRoutes';
 import usersRoutes from './routes/userRoutes';
+import logger from 'morgan';
 
 const app = express();
+
+app.use(logger('dev'));
 app.use(cors());
 app.use(express.json());
 app.use(clientsRoutes);
