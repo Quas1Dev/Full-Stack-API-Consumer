6d0699e2fa33175368b412ec6da2f87f7ba28026
Fernando Bonfim <devnoob@vivaldi.net>
2023-02-09 13:52:59 -0300

    Specified database on link to connect to MongoDB

--- server/src/config/dbConfig.ts
+++ server/src/config/dbConfig.ts
@@ -3,7 +3,7 @@ mongoose.set('strictQuery', false);
 
 const password = process.env.PASSWORD;
 
-const uriToConnect = `mongodb+srv://usuario:${password}@cluster0.tiaomwt.mongodb.net/?retryWrites=true&w=majority`
+const uriToConnect = `mongodb+srv://usuario:${password}@cluster0.tiaomwt.mongodb.net/desafio?retryWrites=true&w=majority`
 
 const connection = mongoose.connect(uriToConnect);
 
